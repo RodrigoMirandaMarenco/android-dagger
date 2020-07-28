d8a489c80d12c51876607b4a147cbfc23faa3dff
Rodrigo Miranda <rmiranda@applaudostudios.com>
2020-07-28 15:16:04 -0600

    ✨ Add Singleton annotation to AppComponent

--- app/src/main/java/com/example/android/dagger/di/AppComponent.kt
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -5,10 +5,12 @@ import com.example.android.dagger.main.MainActivity
 import com.example.android.dagger.registration.RegistrationActivity
 import dagger.BindsInstance
 import dagger.Component
+import javax.inject.Singleton
 
 /**
  * Created by rodrigomiranda on 7/28/20.
  */
+@Singleton
 @Component(modules = [StorageModule::class])
 interface AppComponent {
 
