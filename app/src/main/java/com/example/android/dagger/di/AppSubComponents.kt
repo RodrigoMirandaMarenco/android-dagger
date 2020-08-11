f2df83bddfd482975653cd012c43f1f6a60daccd
Rodrigo Miranda <rmiranda@applaudostudios.com>
2020-08-11 11:52:54 -0600

    ✨ Add LoginComponent to AppSubComponents

--- app/src/main/java/com/example/android/dagger/di/AppSubComponents.kt
+++ app/src/main/java/com/example/android/dagger/di/AppSubComponents.kt
@@ -1,10 +1,11 @@
 package com.example.android.dagger.di
 
+import com.example.android.dagger.login.LoginComponent
 import com.example.android.dagger.registration.RegistrationComponent
 import dagger.Module
 
 /**
  * Created by rodrigomiranda on 7/28/20.
  */
-@Module(subcomponents = [RegistrationComponent::class])
+@Module(subcomponents = [RegistrationComponent::class, LoginComponent::class])
 class AppSubComponents
